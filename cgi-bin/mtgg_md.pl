66eacdaa394eb2b5ae6bebd10ca03615f29a13e2
Mick Gregg <mick@mickgregg.com>
2013-03-05 22:57:18 +0000

    Fixed the CGI script to allow better use with both CGI and command line. Added jQuery to requirements list on the example home page.

--- cgi-bin/mtgg_md.pl
+++ cgi-bin/mtgg_md.pl
@@ -1,13 +1,22 @@
+#!/usr/bin/perl
 use strict;
 use warnings;
 
 use CGI::Carp 'fatalsToBrowser';
-use CGI 'param';
+use CGI ':standard';
 use Text::Markdown 'markdown';
 
+# allow cli use without printing headers
+my $cli = param('cli') ? 1 : 0;
+
+# default to the home page
 my $dirname = 'content';
 my $filename = 'home';
+
+# the page to generate
 $dirname = param 'key' if param('key');
 $filename = param 'value' if param('value');
 open my $fh, '<',  "../$dirname/$filename.md" or die;
+
+print header('text/html; charset=UTF-8') unless $cli;
 print markdown join "\n", <$fh>;
--- content/home.md
+++ content/home.md
@@ -3,6 +3,7 @@
 This is a default home page for the Hungry Horse framework.
 
 You'll need
-* to know [Markdown syntax](http://daringfireball.net/projects/markdown/syntax), which is easy, and
-* have the Perl Text::Markdown module
+* to know [Markdown syntax](http://daringfireball.net/projects/markdown/syntax), which is easy,
+* have the Perl Text::Markdown module, and
+* use jQuery
 
